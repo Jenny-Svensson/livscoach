798917839706f26d397908a26d4923d2416d2bd7
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2023-12-30 15:44:52 +0100

    update styles and scripts enqueue functions for the livscoach theme

--- themes/livscoach/functions.php
+++ themes/livscoach/functions.php
@@ -1,18 +1,18 @@
 <?php 
-    function livscoach_register_styles(){
 
-        /*  Enqueue the main theme stylesheet
-            Ensure that 'livscoach-bootstrap'-stylesheet as a dependency has already been loaded, before loading 'livscoach'-stylesheet  
-        */
+    function livscoach_register_styles(){
         wp_enqueue_style('livscoach', get_template_directory_uri() . "/style.css", array('livscoach-bootstrap'), '1.0', 'all');
-        
-        // Enqueue Font Awesome stylesheet from a CDN
         wp_enqueue_style('livscoach-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
-        
-        // Enqueue Bootstrap CSS framework stylesheet from a CDN
         wp_enqueue_style('livscoach-bootstrap', "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css", array(), '5.3.2', 'all');
     }
+        add_action('wp_enqueue_scripts', 'livscoach_register_styles');
 
-    // Hook the livscoach_register_styles function to the wp_enqueue_scripts action
-    add_action('wp_enqueue_scripts', 'livscoach_register_styles');
+    function livscoach_register_scripts(){
+        wp_enqueue_script('livscoach-jquery', 'https://code.jquery.com/jquery-3.6.0.min.js', array(), '3.6.0', true);
+        wp_enqueue_script('livscoach-popper', 'https://cdn.jsdelivr.net/npm/popper.js@2.9.3/dist/umd/popper.min.js', array(), '2.9.3', true);
+        wp_enqueue_script('livscoach-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js', array(), '5.3.2', true);
+        wp_enqueue_script('livscoach-main', get_template_directory_uri() . "/assets/javascript/main.js", array(), '1.0', true);
+    }
+        add_action('wp_enqueue_scripts', 'livscoach_register_scripts');
+
 ?>
