7036d927d2ca6b2dc30253fc814653916781dbc7
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2024-01-03 12:38:31 +0100

    add dynamic title tag support

--- themes/livscoach/functions.php
+++ themes/livscoach/functions.php
@@ -1,5 +1,11 @@
 <?php 
 
+    function livscoach_theme_support() {
+        add_theme_support('title-tag');
+    }
+
+    add_action('after_setup_theme','livscoach_theme_support');
+
     function livscoach_register_styles(){
         wp_enqueue_style('livscoach', get_template_directory_uri() . "/style.css", array('livscoach-bootstrap'), '1.0', 'all');
         wp_enqueue_style('livscoach-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
