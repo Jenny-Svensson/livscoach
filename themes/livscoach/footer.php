18a94e16bbbe20fc214ffa55f0316cecea122a61
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2024-01-07 20:35:31 +0100

    style the footer and import the same logo from wp as in header

--- themes/livscoach/footer.php
+++ themes/livscoach/footer.php
@@ -1,21 +1,30 @@
-<footer class="footer text-center py-2 theme-bg-dark">
-    <h1>footer</h1>
-<div id="navigation_footer">
-        <?php 
-            wp_nav_menu(array(
-                'menu' => 'footer', 
-                'container' => '', 
-                'theme_location' => 'footer',
-                'items_wrap' => '<ul class="navbar-nav">%3$s</ul>'
-            ));
-        ?>
-    </div>
-       </footer>
-   
-   </div>
-    <?php 
-        wp_footer();
-    ?>
-    
+<footer class="footer text-center py-4 theme-bg-dark">
+    <h1 class="footer__title">Livscoach.nu | Olga Anton</h1>
 
-</html> 
+    <div class="footer__content">
+        <a class="footer__logo pt-lg-4 mb-0" href="index.html">
+            <?php if (function_exists('the_custom_logo')) {
+                $custom_logo_id = get_theme_mod('custom_logo');
+                $logo = wp_get_attachment_image_src($custom_logo_id);
+            }
+            ?>
+            <img class="mb-1 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo">
+        </a>
+
+        <table>
+            <tr>
+                <td>Baragatan 3, 212 28 Malmö</td>
+            </tr>
+            <tr>
+                <td>+46 765 545 34</td>
+            </tr>
+            <tr>
+                <td>olga@livscoach.nu</td>
+            </tr>
+        </table>
+    </div>
+</footer>
+
+<?php
+wp_footer();
+?>
