f367ec0fd2d8d859274d940ba2e2d4b9b71d698b
Jenny Svensson <114919739+Jenny-Svensson@users.noreply.github.com>
2024-01-12 11:11:11 +0100

    remove the title of the page on front-page (only using title on other pages)

--- themes/livscoach/header.php
+++ themes/livscoach/header.php
@@ -46,13 +46,6 @@
 
     </header>
 
-    <div class="main-wrapper">
-        <header class="page-title theme-bg-light text-center gradient py-5">
-            <h1 class="heading">
-                <?php the_title(); ?>
-            </h1>
-        </header>
-    </div>
 
 </body>
 
